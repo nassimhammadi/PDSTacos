d2a18586a115ef0dba691fd83b20e808fdb662a1
laurahollard <laura.hollard@etu.u-pec.fr>
2017-05-14 17:37:38 +0200

    Update Monitorer.java

--- PDS/src/client/IHM/Monitorer.java
+++ PDS/src/client/IHM/Monitorer.java
@@ -11,7 +11,7 @@ public class Monitorer extends JFrame{
 	public Monitorer(){
 		
 	    this.setTitle("Activity of Deposit - Workflow");
-	    this.setSize(500, 500);
+	    this.setSize(700, 700);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setLocationRelativeTo(null);
 	    //On définit le layout à utiliser sur le content pane
@@ -30,16 +30,27 @@ public class Monitorer extends JFrame{
 	       c.addItem("All");  
 	       c.addItem("En cours");  
 	       c.addItem("Terminé");  
+	       c.addItem("En attente");
 	      
 	       c.select("All");  
 	    
 	       nord.add(statut);
 	       nord.add(c);
 	      
-	       JPanel west = new JPanel(new GridLayout(1,2));
-		   west.setBackground(Color.white);
-		   west.setPreferredSize(new Dimension(50,50));
-		    
+	       JPanel centre = new JPanel(new GridLayout(1,2));
+		   centre.setBackground(Color.white);
+		   centre.setPreferredSize(new Dimension(50,50));
+		
+		   
+		 JPanel west = new JPanel();
+		 west.setPreferredSize(new Dimension(50,50));
+		 west.setBorder(new TitledBorder("Liste des véhicules"));
+		 centre.add(west);
+		 
+		 JPanel east = new JPanel();
+		 east.setPreferredSize(new Dimension(50,50));
+		 east.setBorder(new TitledBorder("Workflow"));
+		 centre.add(east);
 	    
 		    JPanel south = new JPanel(new GridLayout(1,1));
 		    south.setBackground(Color.white);
@@ -58,13 +69,12 @@ public class Monitorer extends JFrame{
 		    south.add(dateCJ);
 		    
 		    
-		    
-		    
 	    JPanel panelPrincipal = new JPanel();
         panelPrincipal.setBackground(Color.white);
         panelPrincipal.setLayout(new BorderLayout());
         panelPrincipal.add(nord, BorderLayout.NORTH);
-        panelPrincipal.add(west, BorderLayout.CENTER);
+        panelPrincipal.add(centre, BorderLayout.CENTER);
+     
         panelPrincipal.add(south, BorderLayout.SOUTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	    setContentPane(panelPrincipal);
@@ -79,7 +89,7 @@ public class Monitorer extends JFrame{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Monitorer essai = new Monitorer();
+		Monitorer essais = new Monitorer();
 	}
 
 }
