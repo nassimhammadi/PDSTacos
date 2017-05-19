e8f7db9c9efc14a2dda56537ac62524caa3734bf
Lazare <Lazare@192.168.0.27>
2017-05-19 22:10:25 +0200

    CREATION CLASSES MANQUANTES / DEBUG

---
+++ PDS/src/client/model/ListVehicle.java
@@ -0,0 +1,15 @@
+package client.model;
+
+import java.util.ArrayList;
+
+public class ListVehicle {
+
+	private ArrayList<Vehicule> listV;
+
+	public ArrayList<Vehicule> getListv() {
+		// TODO Auto-generated method stub
+		return listV;
+	}
+	
+}
+
---
+++ PDS/src/serv/model/ListVehicle.java
@@ -0,0 +1,14 @@
+package serv.model;
+
+import java.util.ArrayList;
+
+public class ListVehicle {
+
+	public ListVehicle(ArrayList<Vehicule> a_v) {
+		// TODO Auto-generated constructor stub
+	}
+
+	private ArrayList<Vehicule> listV;
+	
+}
+
